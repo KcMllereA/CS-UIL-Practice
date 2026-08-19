1405e114662202822b6153e27fbf677e236646e6
KcMllereA <aerellmck@gmail.com>
2026-08-19 11:52:25 -0500

    Database setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./main.css";
+import "./index.css";
 import "./utils/index.js";
 
 import App from "./App.jsx";
